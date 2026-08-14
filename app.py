bbdaf96df38ec880aca693ce808ab7682c4da5fd
DevloperRohit-vibe <rohitkumar7860830@gmail.com>
2026-08-14 21:16:26 +0530

    __name__ in flask()

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 import os
+import cv2
 # from dotenv import load_dotenv
 
 # load_dotenv()
 
-app = Flask()
+
+
+app = Flask(__name__)
 CORS(app)
-
-import cv2
-
 harcascade_file_name = os.environ.get("HAARCASCADE_FILE_NAME",False)
-
 
 def opencv():
     cascade = cv2.data.haarcascades
@@ -23,11 +22,11 @@ def opencv():
         print(" File Not Exist")
     # print(f" HaarCadcade abs Path : {os.path.abspath(cascade+haarcascadeFIle)}\n HaarCascade Path Rel : {cascade}")
 
-opencv()
 
 
 if __name__ == "__main__":
     print("server started..")
+    opencv()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
 
